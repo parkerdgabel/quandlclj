7c5d31f3a4c5a8013347099c9b6ac516bd34ce03
Parker Gabel <parker.d.gabel@gmail.com>
2021-12-16 12:37:31 -0700

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to quandlclj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/quandlclj/core_test.clj
@@ -0,0 +1,7 @@
+(ns quandlclj.core-test
+  (:require [clojure.test :refer :all]
+            [quandlclj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
